f5140ec9423a37e7963f34e297ee8df6ab6b239f
Mahmoud Khaled <mahmoud_khld@yahoo.com>
2025-02-09 04:25:12 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-path=$(dirname "$0")
 recipename=$(rofi -dmenu -p "recipe:")
 if [ -n "$recipename" ]; then
     echo "You entered: $recipename"
-    out=$(python "$path"/openEmbedded.py "$recipename" | rofi -dmenu)
+    out=$(scrapeOpenEmbedded -p -s "$recipename" | rofi -dmenu)
     echo "$out" | xclip -selection clipboard
 fi
